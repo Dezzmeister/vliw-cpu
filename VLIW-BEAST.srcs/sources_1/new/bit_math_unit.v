94f3c32a8055a93f29a7c7aa4a49b937731b3af4
Dezzmeister <dezzy3359@gmail.com>
2019-02-01 19:52:19 -0500

    Added a module description

--- VLIW-BEAST.srcs/sources_1/new/bit_math_unit.v
+++ VLIW-BEAST.srcs/sources_1/new/bit_math_unit.v
@@ -9,7 +9,7 @@
 // Project Name: 
 // Target Devices: 
 // Tool Versions: 
-// Description: 
+// Description: Combinational execution unit module that handles simple bit math
 // 
 // Dependencies: 
 // 
